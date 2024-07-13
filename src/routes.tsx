750daaa4ada352e1cc8113dfeb4a72bf7ac61630
Taqi <TaqiRahimi@gmail.com>
2024-07-13 15:11:15 +0430

    Handling Router Errors

---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import GameDetailPage from "./pages/GameDetailPage";
+import Homepage from "./pages/Homepage";
+import Layout from "./pages/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      { path: "games/:id", element: <GameDetailPage /> },
+    ],
+  },
+]);
+
+export default router;
