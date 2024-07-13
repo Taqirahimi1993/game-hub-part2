3ec4dd44560da013517862107ad5838b83d51d11
Taqi <TaqiRahimi@gmail.com>
2024-07-13 10:54:59 +0430

    stored data used zustand

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,24 +1,12 @@
-import {
-  Button,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-} from '@chakra-ui/react';
-import { BsChevronDown } from 'react-icons/bs';
-import usePlatform from '../hooks/usePlatform';
-import usePlatforms, { Platform } from '../hooks/usePlatforms';
-
-interface Props {
-  onSelectPlatform: (platform: Platform) => void;
-  selectedPlatformId?: number;
-}
-
-const PlatformSelector = ({
-  onSelectPlatform,
-  selectedPlatformId,
-}: Props) => {
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatform from "../hooks/usePlatform";
+import usePlatforms from "../hooks/usePlatforms";
+import useGameQueryStore from "../store";
+const PlatformSelector = () => {
   const { data, error } = usePlatforms();
+  const setSelectedPlatformId = useGameQueryStore((s) => s.setPlatformId);
+  const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
   const selectedPlatform = usePlatform(selectedPlatformId);
 
   if (error) return null;
@@ -26,12 +14,12 @@ const PlatformSelector = ({
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms'}
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data?.results.map((platform) => (
           <MenuItem
-            onClick={() => onSelectPlatform(platform)}
+            onClick={() => setSelectedPlatformId(platform.id)}
             key={platform.id}
           >
             {platform.name}
---
+++ src/store.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+interface GameQuery {
+  genreId?: number;
+  platformId?: number;
+  sortOrder?: string;
+  searchText?: string;
+}
+
+interface gameQueryStore{
+  gameQuery:GameQuery;
+  setSearchText:(searchText:string)=>void;
+  setGenreId:(genreID:number)=>void;
+  setPlatformId:(platformId:number)=>void;
+  setSortOrder:(sortOrder:string)=>void;
+
+}
+
+const useGameQueryStore=create<gameQueryStore>(set=>({
+  gameQuery:{},
+  setSearchText:(searchText)=>set(()=>({gameQuery:{searchText}})),  
+  setGenreId:(genreId) =>set(store=>({gameQuery:{...store.gameQuery,genreId}})),
+  setPlatformId:(platformId)=>set(store=>({gameQuery:{...store.gameQuery,platformId}})),
+  setSortOrder:(sortOrder)=>set(store=>({gameQuery:{...store.gameQuery,sortOrder}}))
+      
+  
+      
+  
+}))
+
+
+export default useGameQueryStore;
